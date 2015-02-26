2dea4704c81f0b0596ce7a21467af63278283d72
Archit Gupta <architgupta.1993@gmail.com>
2015-02-26 12:46:17 +0530

    Hearwall Gaussian BFS b+tree Dwt2d up-to-date

--- dwt2d/run.sh
+++ dwt2d/run.sh
@@ -1,3 +1,3 @@
-./dwt2d 192.bmp -d 192x192 -f -5 -l 3 > Trace_192.log
+./dwt2d 192.bmp -d 192x192 -f -5 -l 3
 ls
-./dwt2d rgb.bmp -d 1024x1024 -f -5 -l 3 > Trace_rgb.log
+./dwt2d rgb.bmp -d 1024x1024 -f -5 -l 3
